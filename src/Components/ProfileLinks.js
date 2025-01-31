9ace13b594ec83fbaf6b4798c6590bcbd8fb2078
Annette <annetaliya@gmail.com>
2025-01-31 10:05:45 -0800

    image optimization

--- src/Components/ProfileLinks.js
+++ src/Components/ProfileLinks.js
@@ -1,32 +1,33 @@
 import React from "react";
 import "./profileLinks.css";
 
-
 const link = [
   {
     item: "https://landing-page-i6zw.vercel.app/",
-    github: 'https://github.com/Annetaliya/Landing-Page',
+    github: "https://github.com/Annetaliya/Landing-Page",
     title: "Sample landing page",
-    photo: 'https://lh3.googleusercontent.com/d/1m-clqocWaseCcEDDoKXn4Q9-jYL1K45K=s220?authuser=0'
+    photo:
+      "https://lh3.googleusercontent.com/d/1m-clqocWaseCcEDDoKXn4Q9-jYL1K45K=s220?authuser=0",
   },
   {
     item: "https://659968189ed589e081611c57--jocular-valkyrie-82c7de.netlify.app/",
-    github: 'https://github.com/Annetaliya/React-ecommerce',
+    github: "https://github.com/Annetaliya/React-ecommerce",
     title: "E-Commerce Website",
-    photo: 'https://lh3.googleusercontent.com/d/1FPxAUkC4HDTHW4dvyoAiBuO-Up1HDsAj=s220?authuser=0'
+    photo:
+      "https://lh3.googleusercontent.com/d/1FPxAUkC4HDTHW4dvyoAiBuO-Up1HDsAj=s220?authuser=0",
   },
   {
     item: "#home",
     github: "#home",
     title: "Sample Project",
-    photo: 'https://lh3.googleusercontent.com/d/1fh4JQGs0CjtHkG4d7QmXxlbAK_7WRQkz=s220?authuser=0'
+    photo:
+      "https://lh3.googleusercontent.com/d/1fh4JQGs0CjtHkG4d7QmXxlbAK_7WRQkz=s220?authuser=0",
   },
 ];
 
 function ProfileLinks() {
-  
   return (
-    <div id='portfolio'>
+    <div id="portfolio">
       <p className="intro">My Portfolio</p>
       <p className="intro-paragraph">
         I take pride in paying attention to the smallest details and making sure
@@ -37,19 +38,25 @@ function ProfileLinks() {
         {link.length !== 0
           ? link.map((links, index) => (
               <div key={index} className="proLink">
-                  <img src={links.photo} alt='website' loading='lazy'/>
-                  
-                    <a href={links.item} target="_blank" rel="noreferrer">
-                      <button className="btn1">Website</button>
-                    </a>
-                    <a href={links.github} target="_blank" rel="noreferrer">
-                      <button className="btn2">Github</button>
-                    </a>
+                <img
+                  src={links.photo}
+                  alt="website"
+                  loading="lazy"
+                  height="200px"
+                  style={{objectFit: 'cover'}}
+                />
+
+                <a href={links.item} target="_blank" rel="noreferrer">
+                  <button className="btn1">Website</button>
+                </a>
+                <a href={links.github} target="_blank" rel="noreferrer">
+                  <button className="btn2">Github</button>
+                </a>
                 <p className="links-title">{links.title}</p>
               </div>
             ))
           : null}
-        </div>
+      </div>
     </div>
   );
 }
